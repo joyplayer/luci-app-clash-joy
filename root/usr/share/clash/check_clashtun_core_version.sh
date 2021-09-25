75e0552842dfab16601c5100a55fa90684659ccc
joyplayer <joyplayer@126.com>
2021-09-25 23:55:14 +0800

    1）Fix check script

--- root/usr/share/clash/check_clashtun_core_version.sh
+++ root/usr/share/clash/check_clashtun_core_version.sh
@@ -2,7 +2,7 @@
 
 . /usr/share/clash/setproxy.sh
 
-new_clashtun_core_version=`curl -sL ${clash_curl_proxy} "https://github.com/frainzy1477/clashtun/tags" | grep "href=\"/frainzy1477/clashtun/releases/" | head -n 1 | awk -F "/tag/" '{print $2}' | sed 's/\">//'`
+new_clashtun_core_version=`curl -sL ${clash_curl_proxy} "https://github.com/frainzy1477/clashtun/tags"| grep "a href=\"/frainzy1477/clashtun/releases" | head -n 1| awk -F "/tag/" '{print $2}'| sed 's/\">//'`
 if [ "$?" -eq "0" ]; then
 rm -rf /usr/share/clash/new_clashtun_core_version
 if [ $new_clashtun_core_version ]; then
--- root/usr/share/clash/check_luci_version.sh
+++ root/usr/share/clash/check_luci_version.sh
@@ -2,12 +2,12 @@
 
 . /usr/share/clash/setproxy.sh
 
-new_version=`curl -sL ${clash_curl_proxy} "https://gitee.com/joyplayer/lede_third_package/tags"| grep "href=\"/joyplayer/lede_third_package/tree/"| head -n 1| awk -F "/tree/" '{print $2}'| sed 's/\">.*//'`
+new_luci_version=`curl -sL ${clash_curl_proxy} "https://github.com/joyplayer/luci-app-clash-joy/tags"| grep "a href=\"/joyplayer/luci-app-clash-joy/releases/"| head -n 1| awk -F "/tag/" '{print $2}'| sed 's/\">//'`
 if [ "$?" -eq "0" ]; then
 rm -rf /usr/share/clash/new_luci_version
-if [ $new_version ]; then
-echo $new_version > /usr/share/clash/new_luci_version 2>&1 & >/dev/null
-elif [ $new_version =="" ]; then
+if [ $new_luci_version ]; then
+echo $new_luci_version > /usr/share/clash/new_luci_version 2>&1 & >/dev/null
+elif [ $new_luci_version =="" ]; then
 echo 0 > /usr/share/clash/new_luci_version 2>&1 & >/dev/null
 fi
 fi
